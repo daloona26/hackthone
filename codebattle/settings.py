758c69ba0bab3793bd4606527524269dcf5cf463
ahmad <daloona26@gmail.com>
2024-02-25 06:35:41 +0200

    added pg database

--- codebattle/settings.py
+++ codebattle/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mrw396-g-lh4r%zi3!i&30t4#21w!6op6g-tbzwe$0uo^n*2en'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 AUTH_USER_MODEL = 'base.User'
 
